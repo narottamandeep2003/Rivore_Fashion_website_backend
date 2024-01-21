6073f3aa1a0f1b399fbc1756c73c695f79283ac2
Mandeep Narotta <narotta@gmail.com>
2024-01-21 14:23:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { connectDB } from "./db/index.js";
 dotenv.config({ path: "./.env" });
 import app from "./app.js";
 const port = process.env.PORT || 4000;
+
 connectDB().then(async () => {
   mongoose.connection.on("connected", () => {
     console.log("Connect");
